5c7207ca83b3b0fc9f6ff97fac93b52c04a6bda6
David Schwehr <dpschwehr@gmail.com>
2015-12-31 13:26:26 -0700

    last commit before editing files wtih atmel studio

--- lcd.c
+++ lcd.c
@@ -10,11 +10,16 @@
 ******************************************************/
  
 
- /**
- * includes
- */
+/**
+* includes
+*/
+#include <avr/io.h>
+#include <util/delay.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lcd.h"
 
- /**
+/**
 * constants/macros
 */
 
@@ -25,7 +30,14 @@
 /**
 * local functions
 */
+void setupLCDPorts(){
+	LCD_CONTROL |= ((1 << LCD_RS) | (1 << LCD_ENABLE)); // set pins as output
+	LCD_DATA |= ((1 << D7) | (1 << D6) | (1 << D5) | (1 << D4)); // LCD data lines as output
+}  // end setupLCDPorts()
 
+void pulseEnable(){
+	
+}  // end pulseEnable()
 /**
 * public functions
 */
